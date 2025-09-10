056aab1b2ab44af8ba60d099698566761f0e7b95
Jeffrey Shulkin <shulkinj@umich.edu>
2025-09-10 12:58:45 -0400

    Refactored video metrics to work in GPU

--- frame_gen/common/metrics/video_metrics.py
+++ frame_gen/common/metrics/video_metrics.py
@@ -1,52 +1,83 @@
 '''
 This file serves as a dumping ground for all standard video metric calculations.
 '''
-from skimage.metrics import peak_signal_noise_ratio
-from skimage.metrics import structural_similarity
-from skimage.metrics import mean_squared_error
-from lpips import LPIPS
+from typing import Callable, Sequence, Union
+
 import torch
 
-class VideoMetrics:
-    def __init__(self, batch_size=16):
-        self.lpips = LPIPS(net="alex")
-        self.lpips_device = next(self.lpips.parameters()).device
-        self.batch_size = batch_size
+from ignite.metrics import PSNR, SSIM, MeanSquaredError
+from ignite.metrics import Metric, reinit__is_reduced, sync_all_reduce
+from ignite.exceptions import NotComputableError
 
-    def batched_psnr(self, gt_im=None, im=None, data_range=255) -> list[float]:
-        psnrs = []
-        for sample in range(self.batch_size):
-            curr_gt_im = gt_im[sample].transpose(1, 2, 0)
-            curr_pred_im = im[sample].transpose(1, 2, 0)
-            psnrs.append(peak_signal_noise_ratio(image_true=curr_gt_im, image_test=curr_pred_im, data_range=data_range))
+from lpips import LPIPS
 
-        return psnrs
-    
-    def batched_ssim(self, gt_im=None, im=None, data_range=255) -> list[float]:
-        ssims = []
-        for sample in range(self.batch_size):
-            curr_gt_im = gt_im[sample].transpose(1, 2, 0)
-            curr_pred_im = im[sample].transpose(1, 2, 0)
-            ssims.append(structural_similarity(im1=curr_gt_im, im2=curr_pred_im, data_range=data_range, channel_axis=2))
+class LPIPS(Metric):
+    _state_dict_all_req_keys = ("_sum_of_batchwise_lpips", "_num_examples")
 
-        return ssims
-    
-    def batched_lpips(self, gt_im=None, im=None) -> list[float]:
-        curr_gt_im_tensor = torch.from_numpy(gt_im).float().to(self.lpips_device)
-        curr_pred_im_tensor = torch.from_numpy(im).float().to(self.lpips_device)
+    def __init__(
+        self, 
+        output_transform: Callable =lambda x: x,
+        device: Union[str, torch.device] = torch.device("cpu"),
+        skip_unrolling: bool = False
+    ):
+        super(LPIPS, self).__init__(output_transform=output_transform, device=device, skip_unrolling=skip_unrolling)
+        self.lpips_model = LPIPS(net="alex")
 
-        if curr_pred_im_tensor.max() <= 1.0 or curr_gt_im_tensor.max() <= 1.0:
-            curr_pred_im_tensor = (curr_pred_im_tensor * 2) - 1
-            curr_gt_im_tensor = (curr_gt_im_tensor * 2) - 1
+    def _check_shape_dtype(self, output: Sequence[torch.Tensor]) -> None:
+        y_pred, y = output
+        if y_pred.dtype != y.dtype:
+            raise TypeError(
+                f"Expected y_pred and y to have the same data type. Got y_pred: {y_pred.dtype} and y: {y.dtype}."
+            )
         
-        lpips = []
-        with torch.no_grad():
-            lpips = self.lpips(in0=curr_gt_im_tensor, in1=curr_pred_im_tensor).cpu().numpy().tolist()
+        if y_pred.shape != y.shape:
+            raise ValueError(
+                f"Expected y_pred and y to have the same shape. Got y_pred: {y_pred.shape} and y: {y.shape}."
+            )
+        
+    @reinit__is_reduced
+    def reset(self):
+        self._sum_of_batchwise_lpips = torch.tensor(0.0, dtype=self._double_dtype, device=self._device)
+        self._num_examples = 0
 
-        return lpips
-    
-    def mse(self, gt_im, im) -> list[float]:
-        return mean_squared_error(im, gt_im)
-    
-    def vmaf(self, im) -> float:
-        pass
+    @reinit__is_reduced
+    def update(self, output: Sequence[torch.Tensor]) -> None:
+        self._check_shape_dtype(output)
+        y_pred, y = output[0].detach(), output[1].detach()
+
+        dim = tuple(range(1, y.ndim))
+        self._sum_of_batchwise_lpips = torch.sum(self.lpips_model(y_pred, y)).to(
+            device=self.device
+        )
+        self._num_examples += y.shape[0]
+
+    @sync_all_reduce("_sum_of_batchwise_lpips", "_num_examples")
+    def compute(self):
+        if self._num_examples == 0:
+            raise NotComputableError("PSNR must have at least one example before it can be computed.")
+        return (self._sum_of_batchwise_lpips / self._num_examples).item()
+
+class VideoMetrics:
+    def __init__(self, device, data_range=1.0) -> None:
+        self.psnr_metric = PSNR(data_range=data_range, device=device)
+        self.ssim_metric = SSIM(data_range=data_range, device=device)
+        self.lpips_metric = LPIPS(device=device)
+        self.mse_metric = MeanSquaredError(device=device)
+
+    def reset(self):
+        self.psnr_metric.reset()
+        self.ssim_metric.reset()
+        self.lpips_metric.reset()
+        self.mse_metric.reset()
+        
+    def update(self, predicted_frame, gt_frame) -> None:
+        self.psnr_metric.update((predicted_frame, gt_frame))
+        self.ssim_metric.update((predicted_frame, gt_frame))
+        self.lpips_metric.update((predicted_frame, gt_frame))
+        self.mse_metric.update((predicted_frame, gt_frame))
+
+    def compute(self) -> None:
+        self.psnr_metric.compute()
+        self.ssim_metric.compute()
+        self.lpips_metric.compute()
+        self.mse_metric.compute()
